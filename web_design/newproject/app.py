7e7cd677096d2e20fb0bfe5e8b1e6dcfedbe0423
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-03-27 19:08:44 +0530

    added templates for new project

--- web_design/newproject/app.py
+++ web_design/newproject/app.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',title="HomePage")
+
+@app.route("/login/",methods=["POST"])
+def login():
+    username = request.form['name']
+    password = request.form['password']
+    return render_template('login.html',title='login',name=username,password=password)
 
 if __name__ == "__main__" : 
 
-    app.run()
+    app.run(debug=True)
