b4ab4a489393ce7ae630470f56f9102c480b4866
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-03-28 18:03:25 +0530

    added code for jinja templating

--- web_design/newproject/app.py
+++ web_design/newproject/app.py
@@ -8,9 +8,30 @@ def index():
 
 @app.route("/login/",methods=["POST"])
 def login():
-    username = request.form['name']
-    password = request.form['password']
-    return render_template('login.html',title='login',name=username,password=password)
+    data = {
+    'username' : request.form['name'],
+    'password' : request.form['password'], } 
+    return render_template('login.html',title='login',data=data)
+
+@app.route("/signup/")
+def signup_page():
+    return render_template('signup.html',title='signup')
+
+
+@app.route('/mk_signup/',methods=['POST'])
+def signup():
+    data = {
+    "First Name"  : request.form['fname'],
+    "Last Name" :  request.form['lname'],
+    "UserName" : request.form['name'],
+    "Password": request.form['password'],
+    "Email" :  request.form['email'],
+    "Phone Number" : request.form['ph_no'],
+    "Date of Birth" : request.form['dob'],
+    }
+    return render_template('login.html',title='Profile',data=data)
+
+
 
 if __name__ == "__main__" : 
 
