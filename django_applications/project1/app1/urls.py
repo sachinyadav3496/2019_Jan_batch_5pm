3a7a7faaaa75313e1f2ab08ae009b3e3ed4cbcd2
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-04-09 18:53:18 +0530

    added code for django project1

--- django_applications/project1/app1/urls.py
+++ django_applications/project1/app1/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('data/',views.data),
+    path('signup/',views.signup),
+    path('login/',views.login),
+    path('mk_signup/',views.mksignup),
 ]
--- django_applications/project1/app1/views.py
+++ django_applications/project1/app1/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return render(request,'app1/index.html')
+    e = "Please Sign In to Enjoy Our Services"
+    return render(request,'app1/index.html',{'error':e})
 
 
 def data(request):
     return render(request,'app1/data.html')
+
+def signup(request):
+    return render(request,"app1/signup.html")
+
+def login(request):
+    return HttpResponse("Sucess")
+
+def mksignup(request):
+    return HttpResponse("Sucess")
