55f95c9e0fd6f75ed12589aaa5c231bb96067afb
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-04-09 17:40:13 +0530

    added url for static files

--- django_applications/project1/project1/settings.py
+++ django_applications/project1/project1/settings.py
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT=''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR,'app1/static'),
+    os.path.join(BASE_DIR,'app2/static'),
+     ]
